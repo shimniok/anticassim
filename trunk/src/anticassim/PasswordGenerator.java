839aa70921dbbb2e79233a32afe332c4635a6e99
shimniok <shimniok@4a9e5116-b3f1-ea5f-947f-65692ef88a3a>
2010-09-12 06:13:17 +0000

    Initial commit

---
+++ trunk/src/anticassim/PasswordGenerator.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package anticassim;
+
+/**
+ *
+ * @author mes
+ */
+public interface PasswordGenerator {
+    public abstract String generate(int passLength);
+}
